3f52cd1f3a436592e76324ffc3dc3bee0412fe55
MedOTHMAN <mohamed.n.othman@gmail.com>
2021-04-17 18:39:18 +0100

    V1.0.1

---
+++ src/components/Movie.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Movie = () => {
+    return (
+        <div className="movie">
+            this is a movie component
+        </div>
+    )
+}
+
+export default Movie
